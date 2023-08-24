1cc6dc60a565629acb744ddf36a5375aab4af86c
TY0018 <111295767+TY0018@users.noreply.github.com>
2023-08-25 01:04:49 +0800

    removed inline kb buttons

--- tele_web_app/app.py
+++ tele_web_app/app.py
@@ -15,27 +15,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def onboard(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=ONBOARD_TEXT)
     
-# async def new_msg(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-#     """Sends a message with three inline buttons attached."""
-#     keyboard = [
-#         [
-#             InlineKeyboardButton("Yes, I would love to!", callback_data="1"), //send scam msg to be checked
-#             InlineKeyboardButton("Another day :(", callback_data="2"),
-#         ],
-#     ]
-
-#     reply_markup = InlineKeyboardMarkup(keyboard)
-
-#     await update.message.reply_text("New Message Received. Would you like to assess it?", reply_markup=reply_markup)
-    
-# async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-#     """Parses the CallbackQuery and updates the message text."""
-#     query = update.callback_query
-
-#     await query.answer()
-
-#     await query.edit_message_text(text=f"Selected option: {query.data}")
-    
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
 
@@ -49,6 +28,6 @@ if __name__ == '__main__':
     application.add_handler(start_handler)
     application.add_handler(onboard_handler)
     application.add_handler(unknown_handler)
-    # application.add_handler(CallbackQueryHandler(button))
+    
     
     application.run_polling()
